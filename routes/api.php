2ebb46344e4910838194035666a29eb2be4033a2
nadws <nandw567@gmail.com>
2025-12-30 11:50:11 +0800

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,20 @@ use App\Http\Controllers\Api\PosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
+Route::get('/pos/{slug}/menu', [PosController::class, 'getMenu']);
+// Simpan Pesanan
+Route::post('/pos/{slug}/checkout', [PosController::class, 'storeOrder']);
+Route::post('/orders/{id}/ready', [PosController::class, 'markAsReady']);
+Route::get('/pos/{slug}/kitchen', [PosController::class, 'getKitchenOrders']);
+// Ganti route markReady yg lama, atau tambah baru khusus item
+Route::post('/order-items/{id}/ready', [PosController::class, 'markItemReady']);
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/pos/{slug}/reports', [PosController::class, 'getReports']);
 
 // Route Logout (Harus punya Token)
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
@@ -19,18 +30,4 @@ Route::middleware('auth:sanctum')->get('/my-stores', function (Request $request)
         // Ambil semua toko milik user yang login
         'stores' => $request->user()->stores
     ]);
-
-    Route::get('/pos/{slug}/reports', [PosController::class, 'getReports']);
-
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    })->middleware('auth:sanctum');
-
-    Route::get('/pos/{slug}/menu', [PosController::class, 'getMenu']);
-    // Simpan Pesanan
-    Route::post('/pos/{slug}/checkout', [PosController::class, 'storeOrder']);
-    Route::post('/orders/{id}/ready', [PosController::class, 'markAsReady']);
-    Route::get('/pos/{slug}/kitchen', [PosController::class, 'getKitchenOrders']);
-    // Ganti route markReady yg lama, atau tambah baru khusus item
-    Route::post('/order-items/{id}/ready', [PosController::class, 'markItemReady']);
 });
